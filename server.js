e0eb920d72ed186313f6f0408d068232885bd2cd
talco1 <talco9711@gmail.com>
2021-09-08 19:30:09 +0300

    Add try-catch blocks

--- server.js
+++ server.js
@@ -9,73 +9,81 @@ var cases_api = "https://covid-api.mmediagroup.fr/v1/cases?country="
 var history_api = "https://covid-api.mmediagroup.fr/v1/history?country="
 
 app.get('/:country/:date', async (req, res) => {
-    //convert first letter to upper case
-    var country_name = req.params.country[0].toUpperCase() + req.params.country.slice(1)
-    //save cases and history paths
-    var cases = cases_api + country_name
-    var history = history_api + country_name + "&status=Confirmed"
-    //reverse date
-    var input_date = reverseDate(req.params.date)
-    //save the data of history route
-    var dates = await getHistory(history)
-    var keys = Object.keys(dates)
+    try { //try-catch block to handle an error in the asynchronous function
+        //convert first letter to upper case
+        var country_name = req.params.country[0].toUpperCase() + req.params.country.slice(1)
+        //save cases and history paths
+        var cases = cases_api + country_name
+        var history = history_api + country_name + "&status=Confirmed"
+        //reverse date
+        var input_date = reverseDate(req.params.date)
+        //save the data of history route
+        var dates = await getHistory(history)
+        var keys = Object.keys(dates)
 
-    var response = "Querying about " + country_name + " " + req.params.date +
-        "<br/> Data:<br/>" + country_name + ", confirmed case " + req.params.date + ": "
-    //check if the input date id today's date
-    if (compareDates(input_date, keys[0])) {
-        var confirmed1 = (await getCases(cases))['confirmed']
-        var confirmed2 = dates[keys[0]]
-        response += confirmed1 + "," + reverseDate(keys[0]) + ": " + confirmed2
-    } else {
-        var confirmed1 = dates[input_date]
-        response += confirmed1
-        //find the previous day index in the keys list
-        var prevDateIdx = keys.indexOf(input_date) + 1
-        var confirmed2 = 0
-        //check if the previous day exists in the history
-        if (prevDateIdx < keys.length) {
-            confirmed2 = dates[keys[prevDateIdx]]
-            response += "," + reverseDate(keys[prevDateIdx]) + ": " + confirmed2
+        var response = "Querying about " + country_name + " " + req.params.date +
+            "<br/> Data:<br/>" + country_name + ", confirmed case " + req.params.date + ": "
+        //check if the input date id today's date
+        if (compareDates(input_date, keys[0])) {
+            var confirmed1 = (await getCases(cases))['confirmed']
+            var confirmed2 = dates[keys[0]]
+            response += confirmed1 + "," + reverseDate(keys[0]) + ": " + confirmed2
+        } else {
+            var confirmed1 = dates[input_date]
+            response += confirmed1
+            //find the previous day index in the keys list
+            var prevDateIdx = keys.indexOf(input_date) + 1
+            var confirmed2 = 0
+            //check if the previous day exists in the history
+            if (prevDateIdx < keys.length) {
+                confirmed2 = dates[keys[prevDateIdx]]
+                response += "," + reverseDate(keys[prevDateIdx]) + ": " + confirmed2
+            }
         }
+        var result = parseInt(confirmed1) - parseInt(confirmed2)
+        if (result < 0)
+            result = 0
+        res.send(response + "<br/>Result: " + result.toString())
+    } catch (e) {
+        res.send(e)
     }
-    var result = parseInt(confirmed1) - parseInt(confirmed2)
-    if (result < 0)
-        result = 0
-    res.send(response + "<br/>Result: " + result.toString())
 })
 
-app.get('/:sourceCountry/:targetcountry/:from/:to', async(req, res) => {
-    //convert first letter to upper case
-    var source_country = req.params.sourceCountry[0].toUpperCase() + req.params.sourceCountry.slice(1)
-    var target_country = req.params.targetcountry[0].toUpperCase() + req.params.targetcountry.slice(1)
-    //save cases and history paths
-    var source_cases = await getCases(cases_api + source_country)
-    var target_cases = await getCases(cases_api + target_country)
-    var source_history = history_api + source_country + "&status=Confirmed"
-    var target_history = history_api + target_country + "&status=Confirmed"
-    //reverse dates
-    var from = reverseDate(req.params.from)
-    var to = reverseDate(req.params.to)
+app.get('/:sourceCountry/:targetcountry/:from/:to', async (req, res) => {
+    try { //try-catch block to handle an error in the asynchronous function
+        //convert first letter to upper case
+        var source_country = req.params.sourceCountry[0].toUpperCase() + req.params.sourceCountry.slice(1)
+        var target_country = req.params.targetcountry[0].toUpperCase() + req.params.targetcountry.slice(1)
+        //save cases and history paths
+        var source_cases = await getCases(cases_api + source_country)
+        var target_cases = await getCases(cases_api + target_country)
+        var source_history = history_api + source_country + "&status=Confirmed"
+        var target_history = history_api + target_country + "&status=Confirmed"
+        //reverse dates
+        var from = reverseDate(req.params.from)
+        var to = reverseDate(req.params.to)
     
-    var response = "Querying about " + source_country + ", " + target_country + " from " + req.params.from +
-        " - to " + req.params.to + "<br/> Data:<br/>"
+        var response = "Querying about " + source_country + ", " + target_country + " from " + req.params.from +
+            " - to " + req.params.to + "<br/> Data:<br/>"
     
-    //get confirmed cases of the countries
-    var confirmed_source = await getConfirmedCases(from, to, source_history, source_cases)
-    var confirmed_target = await getConfirmedCases(from, to, target_history, target_cases)
+        //get confirmed cases of the countries
+        var confirmed_source = await getConfirmedCases(from, to, source_history, source_cases)
+        var confirmed_target = await getConfirmedCases(from, to, target_history, target_cases)
     
-    var source_pop = source_cases['population'], target_pop = target_cases['population']
+        var source_pop = source_cases['population'], target_pop = target_cases['population']
 
-    response += source_country + ", population: " + source_pop + ", confirmed case " +
-        req.params.to + ": " + confirmed_source[1] + "," + req.params.from + ": " + confirmed_source[0] +
-        "<br/>" + target_country + ", population: " + target_pop + ", confirmed case " + req.params.to +
-        ": " + confirmed_target[1] + "," + req.params.from + ": " + confirmed_target[0] + "<br/>"
+        response += source_country + ", population: " + source_pop + ", confirmed case " +
+            req.params.to + ": " + confirmed_source[1] + "," + req.params.from + ": " + confirmed_source[0] +
+            "<br/>" + target_country + ", population: " + target_pop + ", confirmed case " + req.params.to +
+            ": " + confirmed_target[1] + "," + req.params.from + ": " + confirmed_target[0] + "<br/>"
     
-    var result = [((confirmed_source[1] / source_pop) - (confirmed_target[1] / target_pop)).toFixed(3),
+        var result = [((confirmed_source[1] / source_pop) - (confirmed_target[1] / target_pop)).toFixed(3),
         ((confirmed_source[0] / source_pop) - (confirmed_target[0] / target_pop)).toFixed(3)]
     
-    res.send(response + "Result: " + result.toString())
+        res.send(response + "Result: " + result.toString())
+    } catch (e) {
+        res.send(e)
+    }
 })
 
 //deal with wrong input
